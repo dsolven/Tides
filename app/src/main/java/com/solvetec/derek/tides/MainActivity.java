d5317f3303a780126b962f28c1fd858ac61e14af
Derek Solven <dsolven@gmail.com>
2017-10-17 23:39:43 -0700

    Add ksoap2 library as repository. build.gradle: Add maven url.

--- app/src/main/java/com/solvetec/derek/tides/MainActivity.java
+++ app/src/main/java/com/solvetec/derek/tides/MainActivity.java
@@ -3,11 +3,15 @@ package com.solvetec.derek.tides;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.ksoap2.serialization.SoapObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SoapObject request = new SoapObject();
     }
 }
